d031ca567967f9270a855d29dc0a90f60938cb83
pankajkori81 <mahikori8169@gmail.com>
2026-07-05 03:00:07 +0530

    new update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,11 +24,11 @@ const Hero = () => {
           colors={['#A6C8FF', '#5227FF', '#FF9FFC']}
           backgroundColor="#0A29FF"
           speed={0.5}
-          streakCount={1}
+          streakCount={2}
           streakWidth={1}
           streakLength={1}
-          glow={0.5}
-          density={0.3}
+          glow={0.8}
+          density={0.4}
           twinkle={1}
           zoom={3}
           backgroundGlow={0.5}
